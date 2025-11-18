7e2f26291a7738d9f40cecba48ac4c601b1789a5
24ftt1804 <24ftt1804@student.pb.edu.bn>
2025-11-18 15:03:13 +0800

    Add files via upload

---
+++ P4_24FTT1804.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class P4_24FTT1804 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // //4.1 MathQuiz
+        // int num1 = (int)(Math.random() *101);
+        // int num2 = (int)(Math.random() * 101);
+
+        // System.out.println("What is the sum of " + num1 + " + " + num2 + "?");
+        // int sumAns = input.nextInt();
+
+        // boolean checkSum = (sumAns == num1+num2);
+        // System.out.println(num1 + " + " + num2 + " = " + sumAns + " is " + checkSum );
+
+        // System.out.println("What is " + num1 + " % " + num2 + " ? ");
+        // int modulusAns = input.nextInt();
+
+        // boolean checkModulus = (modulusAns == num1%num2);
+        // System.out.println(num1 + " % " + num2 + " = " + modulusAns + " is " + checkModulus);
+
+
+        // //4.2 AreaOfTriangleGeometry
+        // System.out.print("Enter three coordinates for a triangle: ");
+        // double x1 = input.nextDouble();
+        // double y1 = input.nextDouble();
+
+        // double x2 = input.nextDouble();
+        // double y2 = input.nextDouble();
+
+        // double x3 = input.nextDouble();
+        // double y3 = input.nextDouble();
+        
+        
+        // double side1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        // double side2 = Math.sqrt(Math.pow(x3-x2, 2) + Math.pow(y3-y2, 2));
+        // double side3 = Math.sqrt(Math.pow(x1-x3, 2) + Math.pow(y1-y3, 2));
+        
+        
+        // double calc = (side1 + side2 + side3)/2;
+        // double area = Math.sqrt(calc* (calc-side1)*(calc-side2)*(calc-side3));
+        // System.out.printf("The area of the triangle is %.1f\n", area);
+
+
+        // //4.3 ReverseOfInteger
+        // System.out.print("Enter an integer between 100 and 999: ");
+        // int integer = input.nextInt();
+
+        // int hundred = integer/100;
+        // int ten = (integer/10)%10;
+        // int units = integer%10;
+
+        // int reversed = units * 100+ten  * 10+hundred;
+        // System.out.println("The reverse of " +integer+ " is " +reversed);
+
+
+        // //4.4 MoneyChanger
+        // System.out.print("Enter 1 to convert bnd to rm and 2 vise versa: ");
+        // int choice = input.nextInt();
+
+        // System.out.print("Enter the amount to be converted: $");
+        // double amount = input.nextDouble();
+
+        // double result;
+        // if (choice == 1) {
+        //     result = amount * 3;
+        // }else {
+        //     result = amount / 3;
+        // }
+        // System.out.printf("The change is $%.2f\n" , result); 
+        
+        
+        // //4.5 PriceComparison
+        // System.out.print("Enter the price of the 1st package: $" );
+        // double p1 = input.nextDouble();
+
+        // System.out.print("Enter the weight of the 1st package in gram: ");
+        // double w1 = input.nextDouble();
+
+        // System.out.print("\nEnter the price of the 2nd package: $");
+        // double p2 = input.nextDouble();
+
+        // System.out.print("Enter the weight of the 2nd package in gram: ");
+        // double w2 = input.nextDouble();
+
+        // double value1 = p1/w1;
+        // double value2 = p2/w2;
+
+        // if (value1 < value2) {
+        //     System.out.println("It is worth more to buy the 1st package");
+
+        // } else {
+        //     System.out.println("It is worth more to buy the 2nd package");
+        // }
+    }
+}
