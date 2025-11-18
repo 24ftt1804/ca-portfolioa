a77054a81370812725e51bcf9ce3e3d096d2fb2e
24ftt1804 <24ftt1804@student.pb.edu.bn>
2025-11-18 13:38:38 +0800

    Add files via upload

---
+++ P2_24FTT1804.java
@@ -0,0 +1,42 @@
+public class P2_24FTT1804 {
+    public static void main(String[] args) {
+
+        //2.1 DisplayTwoMessages
+        System.out.println("Welcome to AD2204 Introduction to Programming");
+        System.out.println("This is Practical 2");
+
+        // //2.2 – DisplayPattern.java 
+        // System.out.println("(\\___/)    I am an");
+        // System.out.println("( @w@)       OOO    W      W  L");
+        // System.out.println("/)   )      O   O   W  W  W   L");
+        // System.out.println("^   ^        OOO     W   W    L L L");
+
+        // //2.3 PrintTable
+        // System.out.println("a    a^2    a^3");
+        // System.out.println("1    1      1");
+        // System.out.println("2    4      8");
+        // System.out.println("3    9      27");
+        // System.out.println("4    16     64");
+
+        // //2.4 ComputeExpression2
+        // System.out.println("The result of 7.5 x 2.5 - 1.5 x 3");
+        // System.out.println("              ------------------- is 9.5");
+        // System.out.println("                    4 - 2.5");
+
+        // //2.5 DisplayPattern2
+        // System.out.println("    .=\"\"=.");
+        // System.out.println("  /        \\");
+        // System.out.println(" |  O    O  |");
+        // System.out.println("  \\   /\\   /");
+        // System.out.println("  /'-=\\/=-'\\");
+        // System.out.println(" / /      \\ \\      CHIP CHIP!");
+        // System.out.println(" \\/\\      /\\/");
+        // System.out.println("    '.  .'");
+        // System.out.println("    /|\\/|\\");
+    }
+}
+
+
+
+
+
